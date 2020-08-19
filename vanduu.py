de00a00f02c1494a795f4b2597699ecf72fd243c
deepu-dinesh <dineshkumarchowdary05@gmail.com>
2020-08-19 11:59:19 +0530

    Web page

--- vanduu.py
+++
@@ -1,6 +0,0 @@
-import random
-print(100+random.randint(5,10),end='')
-print(100+random.randint(5,10),end='')
-print(100+random.randint(5,10),end='')
-print(100+random.randint(5,10))
-print("Dinesh Kumar")
